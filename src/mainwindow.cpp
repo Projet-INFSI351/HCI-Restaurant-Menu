fd63a612d2baa7198a8b211856043c8af18f02b0
RomainCroze <croze.romain@gmail.com>
2014-04-09 21:31:31 +0200

    second commit

---
+++ src/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    //ui->setupUi(this);
+    setFixedHeight(1024);
+    setFixedWidth(768);
+    setCentralWidget(new Headband(this,"Entrées"));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
